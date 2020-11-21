0392d91776ec78ade97f18e9db8af0e3d621139e
egesd <eliastsky@gmail.com>
2020-11-21 17:23:36 +0200

    changes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import "./App.css";
 
 function App() {
     const [foodName, setFoodName] = useState("");
     const [days, setDays] = useState(0);
+    const [newFoodName, setNewFoodName] = useState("");
+    const [foodList, setFoodList] = useState([]);
+
+    useEffect(() => {
+        Axios.get("http://localhost:3001/read").then((response) => {
+            setFoodList(response.data);
+        });
+    });
 
     const addToList = () => {
         Axios.post("http://localhost:3001/insert", {
@@ -12,6 +20,10 @@ function App() {
             days: days,
         });
     };
+
+    const updateFood = (id) => {
+        Axios.put('http://localhost:3001/update', {id : id, newFoodName: newFoodName})
+    }
 
     return (
         <div className="App">
@@ -33,6 +45,25 @@ function App() {
                 }}
             ></input>
             <button onClick={addToList}>Add To List</button>
+
+            <h1>Food list</h1>
+
+            {foodList.map((val, key) => {
+                return (
+                    <div key={key} className="food">
+                        <h1> {val.foodName}</h1> <h1> {val.daysSinceIAte}</h1>
+                        <input
+                            type="text"
+                            placeholder="New Food Name..."
+                            onChange={(event) => {
+                                setNewFoodName(event.target.value);
+                            }}
+                        />
+                        <button onClick={() => updateFood(val._id)}>Update</button>
+                        <button>Delete</button>
+                    </div>
+                );
+            })}
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -1,37 +1,50 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
-const FoodModel = require('./models/Food');
+const FoodModel = require("./models/Food");
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://ege:testi123@cluster0.id1xd.mongodb.net/food?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-}, {
-    useUnifiedTopology: true
-});
+mongoose.connect(
+    "mongodb+srv://ege:testi123@cluster0.id1xd.mongodb.net/food?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+    },
+    {
+        useUnifiedTopology: true,
+    }
+);
 
-app.post('/insert', async (req, res) => {
-
+app.post("/insert", async (req, res) => {
     const foodName = req.body.foodName;
     const days = req.body.days;
 
     const food = new FoodModel({
         foodName: foodName,
-        daysSinceIAte: days
-    })
+        daysSinceIAte: days,
+    });
 
     try {
         await food.save();
-        res.send('inserted data');
+        res.send("inserted data");
     } catch (err) {
         console.log(err);
     }
 });
 
+app.get("/read", async (req, res) => {
+    FoodModel.find({}, (err, result) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.send(result);
+    });
+});
+
 app.listen(3001, () => {
-    console.log('Server running on port 3001....')
+    console.log("Server running on port 3001....");
 });
